49680e9b8ad7fe9a527833c98c67941f767165df
Dan Slocombe <dan.slocombe@hotmail.com>
2022-04-01 16:00:08 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+#![allow(unused_parens)]
+
+use gms_binder::*;
+use config_box::file_watcher::FileWatcherConfigBox;
+use crossbeam_epoch::{self as epoch};
+
+use std::ffi::CStr;
+use std::os::raw::c_char;
+
+gms_bind_start!("ld50_lib", "ld50_lib.dll", "lib");
+
+static mut CONFIG_BOX : Option<FileWatcherConfigBox> = None;
+
+// hello
+
+#[no_mangle]
+#[gms_bind]
+pub extern "C" fn dummy() -> f64 {
+    println!("Hello");
+    0.0
+}
+
+#[no_mangle]
+#[gms_bind]
+pub extern "C" fn setup_config_box(path_raw : *const c_char) -> f64 {
+    unsafe {
+        let path = CStr::from_ptr(path_raw).to_str().unwrap();
+        CONFIG_BOX = Some(FileWatcherConfigBox::new(path).expect("Could not create configbox"));
+    }
+
+    0.0
+}
+
+#[no_mangle]
+#[gms_bind]
+pub extern "C" fn config_box_tick() -> f64 {
+    unsafe {
+        let config_box = CONFIG_BOX.as_ref().unwrap();
+        config_box.refresh();
+    }
+
+    0.0
+}
+
+#[no_mangle]
+#[gms_bind]
+pub extern "C" fn config_f64(path_raw : *const c_char) -> f64 {
+    unsafe {
+        let config_box = CONFIG_BOX.as_ref().unwrap();
+        let path = CStr::from_ptr(path_raw).to_str().unwrap();
+        let guard = &epoch::pin();
+        let root = config_box.root(guard);
+        root.get_f32(path) as f64
+    }
+}
+
+gms_bind_end!();
