437974fe0dcf08d491af395146fd9174da026f18
Dan Slocombe <dan.slocombe@hotmail.com>
2022-04-05 23:57:00 +0100

    Cleanup unused stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #![allow(unused_parens)]
 
 use gms_binder::*;
-use config_box::file_watcher::FileWatcherConfigBox;
 use crossbeam_epoch::{self as epoch};
 
 use std::ffi::{CStr, CString};
@@ -9,70 +8,8 @@ use std::os::raw::c_char;
 use std::time::Instant;
 
 static mut GLOBAL_ROPEWORLD: Option<rope_lib::GlobalState> = None;
-static mut CONFIG_BOX : Option<FileWatcherConfigBox> = None;
 
 gms_bind_start!("ld50_lib", "ld50_lib.dll", "lib");
-
-#[no_mangle]
-#[gms_bind]
-pub extern "C" fn dummy() -> f64 {
-    println!("Hello");
-    0.0
-}
-
-#[no_mangle]
-#[gms_bind]
-pub extern "C" fn setup_config_box(path_raw : *const c_char) -> f64 {
-    unsafe {
-        let path = CStr::from_ptr(path_raw).to_str().unwrap();
-        CONFIG_BOX = Some(FileWatcherConfigBox::new(path).expect("Could not create configbox"));
-    }
-
-    0.0
-}
-
-#[no_mangle]
-#[gms_bind]
-pub extern "C" fn config_box_tick() -> f64 {
-    unsafe {
-        let config_box = CONFIG_BOX.as_ref().unwrap();
-        config_box.refresh();
-    }
-
-    0.0
-}
-
-#[no_mangle]
-#[gms_bind]
-pub extern "C" fn config_f64(path_raw : *const c_char) -> f64 {
-    unsafe {
-        let config_box = CONFIG_BOX.as_ref().unwrap();
-        let path = CStr::from_ptr(path_raw).to_str().unwrap();
-        let guard = &epoch::pin();
-        let root = config_box.root(guard);
-        root.get_f32(path) as f64
-    }
-}
-
-#[no_mangle]
-#[gms_bind]
-// Warning leaks
-pub extern "C" fn config_str(path_raw : *const c_char) -> *const c_char  {
-    unsafe {
-        let config_box = CONFIG_BOX.as_ref().unwrap();
-        let path = CStr::from_ptr(path_raw).to_str().unwrap();
-        let guard = &epoch::pin();
-        let root = config_box.root(guard);
-
-        let value_str = root.get_str(path);
-
-        let cstring = CString::new(value_str).unwrap();
-        let cstring_ptr = cstring.as_ptr();
-        std::mem::forget(cstring);
-        cstring_ptr
-    }
-}
-
 
 #[no_mangle]
 #[gms_bind]
